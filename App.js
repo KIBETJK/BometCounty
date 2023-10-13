9beb675fd1946ac80ea462aa726316fdf1d1d224
kibetjk <korirjay@gmail.com>
2023-10-14 01:08:13 +0300

    First

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bomet Banana Farmers App</Text>
+      <Text>BometC Banana Farmers App</Text>
       <StatusBar style="auto" />
     </View>
   );
